1cb1885f86dd4cf1cb67ebbe68ecfb954b7c808c
luowenjun <luowenjun@inman.cc>
2024-05-27 11:09:29 +0800

    httpie ok

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use anyhow::{anyhow, Ok, Result};
+use anyhow::{anyhow, Result};
 use clap::{Parser, Subcommand};
+use colored::*;
+use mime::Mime;
 use reqwest::{header, Client, Response, Url};
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 #[derive(Parser, Debug)]
 #[clap(
     name = "httpie",
@@ -29,7 +31,7 @@ struct GetOpts {
     /// HTTP 请求的 URL  
     #[clap(required = true)]
     #[arg(value_parser = parse_url)]
-    url: Url,
+    url: String,
 }
 
 #[derive(Parser, Debug)]
@@ -45,12 +47,13 @@ struct PostOpts {
 }
 
 #[allow(dead_code)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 struct KvPair {
     k: String,
     v: String,
 }
 
+/// 当我们实现 FromStr trait 后，可以用 str.parse() 方法将字符串解析成 KvPair
 impl FromStr for KvPair {
     type Err = anyhow::Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -70,25 +73,76 @@ fn parse_url(s: &str) -> Result<Url, anyhow::Error> {
     Ok(s.parse()?)
 }
 
+/// 因为我们为 KvPair 实现了 FromStr，这里可以直接 s.parse() 得到 KvPair
 fn parse_kv_pair(s: &str) -> Result<KvPair> {
     Ok(s.parse()?)
+}
+
+async fn get(client: Client, args: &GetOpts) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn post(client: Client, args: &PostOpts) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(args.url.as_str()).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+
+    print!("\n");
+}
+
+/// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+        // 其它 mime type，我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+
+/// 打印整个响应
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+/// 将服务器返回的 content-type 解析成 Mime 类型
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
-    // let client = Client::new();
+    let client = Client::new();
     let result = match opts.subcmd {
-        Subcommands::Get(get) => {
-            println!("GET request to: {}", get.url);
-            // ... 处理 GET 请求
-        }
-        Subcommands::Post(post) => {
-            println!("POST body:{:?}", post.body);
-            // println!("POST request to: {}", post.url);
-            // println!("Body:\n{}", post.body.join("\n"));
-            // ... 处理 POST 请求
-        }
+        Subcommands::Get(ref args) => get(client, args).await?,
+        Subcommands::Post(ref args) => post(client, args).await?,
     };
 
     Ok(result)
